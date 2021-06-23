8325348fa815e5dd404830fc6993e84379fcfeae
Jim Sharples <jamie.sharples@gmail.com>
2021-06-23 21:59:51 +1000

    Added server side log file

--- server/util/Logger.js
+++ server/util/Logger.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+
 class Logger {
     static debugOn = true;
     static debugDepth = 1000;
+    static logFile = null;
+
+    static getLogFile() {
+        if (Logger.logFile === null) {
+            Logger.logFile = fs.createWriteStream(__dirname + '/../log/server.log', {flags: 'w'});
+        }
+        return Logger.logFile;
+    }
 
     static log(message, debugDepth = 5) {
         if ((message === null) || (message === undefined)) return;
@@ -8,6 +18,7 @@ class Logger {
         if (debugDepth > this.debugDepth) return;
         if (this.debugOn) {
             console.log(message);
+            Logger.getLogFile().write(new Date() + ":" + message + '\n');
         }
     }
 
